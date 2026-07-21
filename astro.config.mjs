6d9abcb36a8f36a30bd45cdf0abe527dc02f9f51
BFFonseca <49754867+BFFonseca@users.noreply.github.com>
2026-07-21 16:39:36 +0100

    Update domain for site soft-launch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://debugled.dev', // replace with your actual domain
+  site: 'https://debugled.com', // replace with your actual domain
   vite: {
     plugins: [tailwindcss()],
   },
